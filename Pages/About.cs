f71c7d2eef60d04b8a5b401d45421952d765d2ea
salwa <1496118@cegep-heritage.qc.ca>
2022-03-25 12:06:22 -0400

    added css page privacy

---
+++ Pages/About.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razor_hello.Pages;
+
+public class AboutModel : PageModel
+{
+    private readonly ILogger<AboutModel> _logger;
+
+    public AboutModel(ILogger<AboutModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        Console.WriteLine("Hello World!");
+    }
+    // string msg = "hello";
+    // public void DebugLog(String msg)
+    // {
+
+    //     System.Diagnostics.Debug.WriteLine(msg);
+    // }
+}
+
